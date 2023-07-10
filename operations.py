d85a5527e3422479d3515e22fae6f818ef957b1a
YugayIrina <105415518+YugayIrina@users.noreply.github.com>
2023-07-10 19:28:07 +0300

    Help

--- operations.py
+++ operations.py
@@ -1,1 +1,20 @@
-Здесь функции, add subtract multiply, принимают два параметра и возвращают их 
+# Здесь функции, add subtract multiply, принимают два параметра и возвращают их 
+# импортируем функции сложения и т.д
+# импортируем инпут 
+
+# def run_calculator():
+#     while True:
+#       num1, num2, operator = функция()
+#       if oparator == чему?:
+#         принт(вызов функции с параметрами)
+#       иначеесли оператор ==
+
+# и т .д
+
+
+def run_calculator(a, b):
+    return a+b
+ab = run_calculator(10, 30)
+print(ab)
+    
+        
