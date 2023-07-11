023406627ed9e8ac5c07c9d15ef942489d7b3811
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-07-11 17:06:02 +0400

    Update operations.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- operations.py
+++ operations.py
@@ -1,20 +1,6 @@
-# Здесь функции, add subtract multiply, принимают два параметра и возвращают их 
-# импортируем функции сложения и т.д
-# импортируем инпут 
-
-# def run_calculator():
-#     while True:
-#       num1, num2, operator = функция()
-#       if oparator == чему?:
-#         принт(вызов функции с параметрами)
-#       иначеесли оператор ==
-
-# и т .д
-
-
-def run_calculator(a, b):
+def add(a, b):
     return a+b
-ab = run_calculator(10, 30)
-print(ab)
+
+напиши название параметров по PEP8    
     
         
