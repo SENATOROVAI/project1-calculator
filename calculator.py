91b5cd8310e40f1219324f1a4d91e7eb833ee7cc
YugayIrina <105415518+YugayIrina@users.noreply.github.com>
2023-07-11 13:32:06 +0300

    calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,29 @@
-импортируем функции сложения и т.д
-импортируем инпут 
+# импортируем функции сложения и т.д
+# импортируем инпут 
 
-def run_calculator():
-    while True:
-      num1, num2, operator = функция()
-      if oparator == чему?:
-        принт(вызов функции с параметрами)
-      иначеесли оператор ==
+# def run_calculator():
+#     while True:
+#       num1, num2, operator = функция()
+#       if oparator == чему?:
+#         принт(вызов функции с параметрами)
+#       иначеесли оператор ==
 
-и т .д
+# и т .д
+import math
+import input_data
+
+def run_calculator(num1, num2):
+  while True:
+    if operation == '+':
+       print(num1 + num2)
+    elif operation == '-':
+       print(num1 - num2)
+    elif operation == '*':
+       print(num1 * num2)
+    elif operation == '/':
+       print(num1 / num2)
+    elif operation == '**':
+      print(num1 ** num2)
+    else:
+      print('ERROR')
+     
