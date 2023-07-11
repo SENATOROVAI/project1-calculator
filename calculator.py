03e45a27a2c7e0333c603e0c45b83408bb9fbcde
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-07-11 17:03:07 +0400

    Update calculator.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- calculator.py
+++ calculator.py
@@ -1,29 +1,22 @@
 # импортируем функции сложения и т.д
 # импортируем инпут 
+фром ...
 
 # def run_calculator():
 #     while True:
 #       num1, num2, operator = функция()
-#       if oparator == чему?:
-#         принт(вызов функции с параметрами)
-#       иначеесли оператор ==
+        if operation == '+':
+           print(назввание функции сложения с параметрами)
+        elif operation == '-':
+           print(num1 - num2)
+        elif operation == '*':
+           print(num1 * num2)
+        elif operation == '/':
+           print(num1 / num2)
+        elif operation == '**':
+          print(num1 ** num2)
+        else:
+          print('ERROR')
 
-# и т .д
-import math
-import input_data
 
-def run_calculator(num1, num2):
-  while True:
-    if operation == '+':
-       print(num1 + num2)
-    elif operation == '-':
-       print(num1 - num2)
-    elif operation == '*':
-       print(num1 * num2)
-    elif operation == '/':
-       print(num1 / num2)
-    elif operation == '**':
-      print(num1 ** num2)
-    else:
-      print('ERROR')
      
