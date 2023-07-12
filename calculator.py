37adfa9f5c192afcc045f76f7bb96c7829e0bc02
artemiyasb <vtyurin.artem@bk.ru>
2023-07-12 23:14:06 +0500

    help

--- calculator.py
+++ calculator.py
@@ -1,22 +1,24 @@
+from input_data import input_data
+from operations import subtract
+from divide_zero import divide
+
 def run_calculator():
     while True:
-      num1, num2, operator = input_data()
       
-        if operator == '+':
-           print(num1 + num2)
-        elif operator == '-':
-           print(num1 - num2)
-        elif operator == '*':
-           print(num1 * num2)
-        elif operation == '/':
-                result = divide(num1, num2)
-                if result is not None:
-                    print(num1 / num2)
-        elif operation == '**':
-           print(num1 ** num2)
-        else:
-           print('ERROR')
-           continue 
+         num1, num2, operation = input_data()
 
-
-     
+         # if operation == "+":
+         #    print(num1 + num2)
+         if operation == "-":
+            print(subtract(num1, num2))
+      #   elif operation == "*":
+      #       print(num1 * num2)
+      #   elif operation == "/":
+         #   result = divide(num1, num2)
+         #   if result is not None:
+          #      print(num1 / num2)
+      #  elif operation == "**":
+      #      print(num1**num2)
+      #  else:
+      #      print("ERROR")
+      #      continue
