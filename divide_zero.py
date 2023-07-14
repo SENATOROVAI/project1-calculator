dd057fb9bfb87fab300a5565afabf091c8e930f3
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-07-14 20:58:49 +0400

    Update divide_zero.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- divide_zero.py
+++ divide_zero.py
@@ -1,5 +1,5 @@
 def divide(num1, num2):
-    if not (num1 and num2):
+    if not (num1 and num2): # но ты здесь не проверяешь переменную на 0 
         print("Делить на ноль нельзя")
         return None
     
