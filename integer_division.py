ec076160c504caaa0be8bb9d5d51c5ea76418298
ProNatalya <96188758+ProNatalya@users.noreply.github.com>
2023-10-20 22:26:06 +0300

    Update integer_division.py
    change to ternary operator
    
    Signed-off-by: ProNatalya <96188758+ProNatalya@users.noreply.github.com>

--- integer_division.py
+++ integer_division.py
@@ -1,4 +1,5 @@
 def int_division(num1, num2):
-    if num2 == 0:
-            return None
-        return num1 // num2 # используй тернарный оператор
+   # if num2 == 0:
+   #         return None
+   #     return num1 // num2 # используй тернарный оператор
+     return None  if num2 == 0 else num1 // num2 # тернарный оператор
