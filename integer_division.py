cc6a955bf417a3d44b4b86ee5dfaeddcec4a3648
Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>
2023-10-20 15:54:05 +0400

    Update integer_division.py
    Signed-off-by: Ruslan Senatorov <55090151+ruslansenatorov@users.noreply.github.com>

--- integer_division.py
+++ integer_division.py
@@ -1,6 +1,4 @@
 def int_division(num1, num2):
     if num2 == 0:
-        return None
-    elif num1 == 0:
-        return 0
-    return num1 // num2
+            return None
+        return num1 // num2 # используй тернарный оператор
